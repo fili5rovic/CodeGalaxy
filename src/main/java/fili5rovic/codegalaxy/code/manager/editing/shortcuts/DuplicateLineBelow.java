b00e2fe22efbb53641d17d8d397e4bae1f506190
fili5rovic <fili5rovic@gmail.com>
2025-04-27 20:25:29 +0200

    Changed shortcut behaviour for codegalaxy, now it reads the shortcuts from settings

--- src/main/java/fili5rovic/codegalaxy/code/manager/editing/shortcuts/DuplicateLineBelow.java
+++ src/main/java/fili5rovic/codegalaxy/code/manager/editing/shortcuts/DuplicateLineBelow.java
@@ -1,8 +1,6 @@
 package fili5rovic.codegalaxy.code.manager.editing.shortcuts;
 
 import fili5rovic.codegalaxy.code.CodeGalaxy;
-import fili5rovic.codegalaxy.code.manager.editing.shortcuts.keystate.KeyState;
-import javafx.scene.input.KeyCode;
 
 public class DuplicateLineBelow extends Shortcut {
     public DuplicateLineBelow(CodeGalaxy cg) {
