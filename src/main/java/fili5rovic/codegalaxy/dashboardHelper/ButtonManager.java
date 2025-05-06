0eb519ec7fa669c6afaa68a10217abd1ceee3a6b
fili5rovic <fili5rovic@gmail.com>
2025-05-06 19:17:06 +0200

    Changed coloring of a button

--- src/main/java/fili5rovic/codegalaxy/dashboardHelper/ButtonManager.java
+++ src/main/java/fili5rovic/codegalaxy/dashboardHelper/ButtonManager.java
@@ -4,10 +4,13 @@ import fili5rovic.codegalaxy.controller.DashboardController;
 import fili5rovic.codegalaxy.util.SVGUtil;
 import fili5rovic.codegalaxy.window.Window;
 import javafx.event.ActionEvent;
+import javafx.scene.control.SplitPane;
 
 public class ButtonManager {
 
     private static final DashboardController controller = (DashboardController) Window.getController(Window.WINDOW_DASHBOARD);
+
+    private static double previousSliderValue = 0.2;
 
     public static void initialize() {
         buttonIcons();
@@ -19,10 +22,19 @@ public class ButtonManager {
     }
 
     private static void buttonIcons() {
-        controller.getShowHierarchyBtn().setGraphic(SVGUtil.getUI("openProject", 16, 16));
+        controller.getShowHierarchyBtn().setGraphic(SVGUtil.getUI("expand", 16, 16));
     }
 
     private static void showHierarchy(ActionEvent actionEvent) {
+        SplitPane.Divider divider = controller.getMainSplitPane().getDividers().getFirst();
+        if (divider.getPosition() < 0.01) {
+            divider.setPosition(previousSliderValue);
+            controller.getShowHierarchyBtn().setGraphic(SVGUtil.getUI("collapse", 16, 16));
+        } else {
+            previousSliderValue = divider.getPosition();
+            divider.setPosition(0);
+            controller.getShowHierarchyBtn().setGraphic(SVGUtil.getUI("expand", 16, 16));
+        }
     }
 
 
