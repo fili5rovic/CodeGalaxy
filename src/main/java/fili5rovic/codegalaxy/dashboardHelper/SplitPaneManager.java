82d397c3789f3d4e3dfd2b97c37f4b9c19bce882
fili5rovic <fili5rovic@gmail.com>
2025-05-21 23:59:35 +0200

    Added listener for width changes to match up the sliders in mainSplitPane

--- src/main/java/fili5rovic/codegalaxy/dashboardHelper/SplitPaneManager.java
+++ src/main/java/fili5rovic/codegalaxy/dashboardHelper/SplitPaneManager.java
@@ -9,21 +9,16 @@ import javafx.scene.control.SplitPane;
 public class SplitPaneManager {
     private static final DashboardController controller = (DashboardController) Window.getController(Window.WINDOW_DASHBOARD);
 
-
+    // When you want to add another pane to the right, make it a child of mainSplitPane
     public static void setupLockPositions() {
         Platform.runLater(() -> {
             SplitPane mainSplitPane = controller.getMainSplitPane();
-            System.out.println(mainSplitPane.getWidth());
             mainSplitPane.widthProperty().addListener((observable, oldValue, newValue) -> {
                 ObservableList<SplitPane.Divider> dividers = mainSplitPane.getDividers();
 
                 double ratio = 966.0 / newValue.doubleValue();
-                dividers.get(0).setPosition(0.2 * ratio);
-                dividers.get(1).setPosition(0.97 * ratio);
+                dividers.getFirst().setPosition(0.2 * ratio);
             });
-
-
-
         });
     }
 }
