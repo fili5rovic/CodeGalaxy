bef21d64e6793bd0d14029d5ad6b7294175acd02
fili5rovic <fili5rovic@gmail.com>
2025-06-13 21:33:38 +0200

    Updated UI

--- src/main/java/fili5rovic/codegalaxy/vcs/treeView/GitTreeItem.java
+++ src/main/java/fili5rovic/codegalaxy/vcs/treeView/GitTreeItem.java
@@ -1,14 +1,25 @@
 package fili5rovic.codegalaxy.vcs.treeView;
 
 import javafx.scene.control.ToggleButton;
-import javafx.scene.control.TreeItem;
 
-public class GitTreeItem extends TreeItem<ToggleButton> {
+public class GitTreeItem {
+    private ToggleButton button;
+    private String name;
 
     public GitTreeItem(String name) {
-        super(new ToggleButton(name));
-        getValue().setOnAction(event -> {
+        this.name = name;
+        this.button = new ToggleButton();
+        this.button.getStyleClass().add("git-toggle-button");
+        this.button.setOnAction(event -> {
             System.out.println("Toggle button clicked: " + name);
         });
+    }
+
+    public ToggleButton getValue() {
+        return button;
+    }
+
+    public String getName() {
+        return name;
     }
 }
